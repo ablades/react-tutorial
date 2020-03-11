99f277eedb09a3112c0c7d386d98537c98f3f8a7
ablades <audaris@gmail.com>
2020-03-11 14:26:26 -0700

    begin adding game history

--- src/index.js
+++ src/index.js
@@ -13,36 +13,20 @@ function Square(props) {
 
   /* Passes in value as jsx a property to square Passing properties from parent to child is how react works!*/
   class Board extends React.Component {
-    constructor(props) {
-      super(props);
-      // Sets states to an array of 9 nulls
-      this.state = {squares: Array(9).fill(null)}
-    }
-
-    //Handles clicks on the board changes square at i to an x 
-    handleClick(i) {
-      // Creates copy of squares to modify allows keeping of previous states
-      const squares = this.state.squares.slice();
-      squares[i] = 'X';
-      this.setState({squares:squares});
-    }
 
     renderSquare(i) {
       return (
         <Square 
         // two props onclick -prop function that square calls
-          value={this.state.squares[i]}
-          onClick={() => this.handleClick(i)}
+          value={this.props.squares[i]}
+          onClick={() => this.props.onClick(i)}
         />
       );
     }
   
     render() {
-      const status = 'Next player: X';
-  
       return (
         <div>
-          <div className="status">{status}</div>
           <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
@@ -64,19 +48,82 @@ function Square(props) {
   }
   
   class Game extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        history: [{
+          squares: Array(9).fill(null)
+        }],
+        xIsNext: true,
+      };
+
+    }
+
+    handleClick(i) {
+      const history = this.state.history;
+      const current = history[history.length - 1];
+      const squares = current.squares.slice();
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        history: history.concat ([{
+          squares: squares,
+        }]),
+        xIsNext: !this.state.xIsNext,
+      });
+    }
+
     render() {
+      const history = this.state.history;
+      const current = history[history.length - 1];
+      const winner = calculateWinner(current.squares);
+
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
+
       return (
         <div className="game">
           <div className="game-board">
-            <Board />
+            <Board 
+              squares={current.squares}
+              onClick={(i) => this.handleClick(i)}
+            />
+
           </div>
           <div className="game-info">
-            <div>{/* status */}</div>
+            <div>{status}</div>
             <ol>{/* TODO */}</ol>
           </div>
         </div>
       );
     }
+  }
+
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
   }
   
   // ========================================
@@ -85,3 +132,5 @@ function Square(props) {
     <Game />,
     document.getElementById('root')
   );
+
+  // Taking Turns
